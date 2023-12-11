79e1552813ab7087fbf6ba79e8caf24bf74099fb
AngelaFallas <angelafallascespedes@gmail.com>
2023-12-10 19:26:24 -0600

    Creacion boton de descarga y ajuste de detalles.

--- Practica_programada_3.R
+++ Practica_programada_3.R
@@ -1,12 +1,8 @@
 library(shiny)
-library(ggplot2)
 library(shinydashboard)
-library(dplyr)
-library(readxl)
 library(ggplot2)
-library(gapminder)
+library(dplyr)
 library(readr)
-library(plotly)
 
 datos_spotify <- read.csv2("datos/spotify_2000_2023.csv", header = TRUE)
 
@@ -18,36 +14,48 @@ ui <- dashboardPage(
       selectInput(inputId = "year",
                   label = "Año",
                   choices = unique(datos_spotify$year),
-                  selected = unique(datos_spotify$year)[4]),
+                  selected = unique(datos_spotify$year)[1]),  
       selectInput(inputId = "top.genre",
                   label = "Género",
                   choices = unique(datos_spotify$top.genre),
-                  selected = unique(datos_spotify$top.genre)[3])
+                  selected = unique(datos_spotify$top.genre)[1]),
+      style = "text-align: center;",
+      downloadButton("descargar_datos", "Descargar Datos")
     )
   ),
   dashboardBody(
     fluidRow(
-      column(width = 6, plotOutput("grafico_1")),
-      column(width = 6, tableOutput("table_1"), 
+      column(width = 5, plotOutput("grafico_1")),
+      column(width = 7,  dataTableOutput("table_1"), 
              label = "Tabla")
     )
   )
 )
 
 server <- function(input, output, session) {
+  filtered_data <- reactive({
+    datos_spotify |> 
+      filter(year == input$year, top.genre == input$top.genre)
+  })
+  
   output$grafico_1 <- renderPlot({
-    filtered_data <- datos_spotify[
-      datos_spotify$year == input$year &
-        datos_spotify$top.genre == input$top.genre, ]
-    
-    ggplot(data = filtered_data) +
-      geom_point(mapping = aes(x = bpm, y = popularity)) +
+    ggplot(data = filtered_data(), aes(x = bpm, y = popularity)) +
+      geom_point() +
       theme_classic()
   })
-  top_popularity <- top_n(datos_spotify, n = input)
-  output$table_1 <- renderTable({
-    datos_spotify
+  
+  output$table_1 <- renderDataTable({
+    filtered_data()
   })
+  
+  output$descargar_datos <- downloadHandler(
+    filename = function() {
+      paste("datos_", input$top.genre, "_", input$year, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered_data(), file, row.names = FALSE)
+    }
+  )
 }
 
 shinyApp(ui, server)
